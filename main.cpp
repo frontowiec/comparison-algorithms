cb8d09cf72f154595968a88b77af4520c5b5c391
Marcin Sirocki <marcinsirocki@gmail.com>
2016-08-27 18:35:24 +0200

    passing input data to array

--- main.cpp
+++ main.cpp
@@ -2,7 +2,20 @@
 
 using namespace std;
 
+void heapify(int heap[], int length) {
+    for (int i = 0; i < length; i++) {
+        cout << heap[i] << endl;
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string str;
+    cin >> str;
+    int heap[str.length()];
+    for(int i = 0; i < str.length(); i++) {
+        heap[i] = (int)str[i]-'0';
+    }
+
+    heapify(heap, str.length());
     return 0;
 }
