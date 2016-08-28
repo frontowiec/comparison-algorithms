6dfae7b3359c2091f937c6191b322f3f087a01ee
Marcin Sirocki <marcinsirocki@gmail.com>
2016-08-28 10:38:32 +0200

    heapify

--- main.cpp
+++ main.cpp
@@ -3,8 +3,15 @@
 using namespace std;
 
 void heapify(int heap[], int length) {
-    for (int i = 0; i < length; i++) {
-        cout << heap[i] << endl;
+    for (int i = length-1; i >= 0; i--) {
+        if(i>0) {
+            int parent = heap[(i-1)/2];
+            int child = heap[i];
+            if(parent < child) {
+                heap[i] = parent;
+                heap[(i-1)/2] = child;
+            }
+        }
     }
 }
 
@@ -12,10 +19,16 @@ int main() {
     string str;
     cin >> str;
     int heap[str.length()];
+    for (int i = 0; i < str.length(); i++) {
+        heap[i] = (int) str[i] - '0';
+    }
+    heapify(heap, str.length());
+
     for(int i = 0; i < str.length(); i++) {
-        heap[i] = (int)str[i]-'0';
+        cout << heap[i] << " | ";
     }
 
-    heapify(heap, str.length());
+    cout << endl;
+
     return 0;
 }
