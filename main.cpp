5c2efd05eee24ea0003c1670dbbddc30803044f9
Marcin Sirocki <marcinsirocki@gmail.com>
2016-08-28 12:05:08 +0200

    heap sort

--- main.cpp
+++ main.cpp
@@ -2,18 +2,47 @@
 
 using namespace std;
 
+//TODO: nie wiem jak to uproscic
 void heapify(int heap[], int length) {
-    for (int i = length-1; i >= 0; i--) {
-        if(i>0) {
-            int parent = heap[(i-1)/2];
-            int child = heap[i];
-            if(parent < child) {
-                heap[i] = parent;
-                heap[(i-1)/2] = child;
+    int c = length;
+    while (c >= 0) {
+        for (int i = length; i >= 0; i--) {
+            if (i > 0) {
+                int parent = heap[(i - 1) / 2];
+                int child = heap[i];
+                if (parent < child) {
+                    heap[i] = parent;
+                    heap[(i - 1) / 2] = child;
+                }
             }
         }
+
+        c--;
     }
 }
+
+void heapSort (int heap[], int length) {
+    int sortingHeap[length];
+    int l = length+1;
+    while(length >= 0){
+        heapify(heap, length);
+        sortingHeap[length] = heap[0];
+
+        heap[0] = heap[length];
+        heap[length] = NULL;
+
+        length--;
+    }
+
+    for (int i = 0; i < l; i++) {
+        cout << sortingHeap[i] << " | ";
+    }
+
+    cout << endl;
+
+
+}
+
 
 int main() {
     string str;
@@ -22,13 +51,7 @@ int main() {
     for (int i = 0; i < str.length(); i++) {
         heap[i] = (int) str[i] - '0';
     }
-    heapify(heap, str.length());
-
-    for(int i = 0; i < str.length(); i++) {
-        cout << heap[i] << " | ";
-    }
-
-    cout << endl;
+    heapSort(heap, str.length()-1);
 
     return 0;
 }
